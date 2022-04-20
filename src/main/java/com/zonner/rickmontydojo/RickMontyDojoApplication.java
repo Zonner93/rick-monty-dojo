77cffa5257833974a71138f3956188b0d04042c0
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-04-20 09:32:19 +0200

    initial commit

---
+++ src/main/java/com/zonner/rickmontydojo/RickMontyDojoApplication.java
@@ -0,0 +1,13 @@
+package com.zonner.rickmontydojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickMontyDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickMontyDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zonner/rickmontydojo/RickMontyDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonner.rickmontydojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickMontyDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
